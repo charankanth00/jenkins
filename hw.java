69691e4428d21669d23709bebe8d756a76c635e4
charankanth00 <142967926+charankanth00@users.noreply.github.com>
2025-11-02 13:14:33 +0530

    Remove duplicate print statement and add new one

--- hw.java
+++ hw.java
@@ -3,6 +3,8 @@ class hw
   public static void main(String [] args)
 	{
 	System.out.println("bye world");		
-	System.out.println("bye world");	
+		System.out.println("bye world");
+				System.out.println("tata");
+
 	}
 }
